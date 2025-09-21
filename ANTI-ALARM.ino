5cf1cdf59847697c90ed81e68c3fd7638fc9ca04
dineshkartheek <dineshkartheek2327@gmail.com>
2025-09-21 13:25:17 +0530

    Add files via upload

---
+++ ANTI-ALARM.ino
@@ -0,0 +1,32 @@
+const int blinkPin = 2;     
+const int motorPin = 13;      
+const int buzzerPin = 12;
+long time = 0;
+
+void setup() {
+  pinMode(motorPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(blinkPin, INPUT);
+  digitalWrite(motorPin, HIGH);
+  digitalWrite(buzzerPin, HIGH);
+}
+
+void loop() {
+  if (!digitalRead(blinkPin)) {
+    time = millis();
+    while (!digitalRead(blinkPin)) { 
+      digitalWrite(buzzerPin, LOW);
+      digitalWrite(motorPin, LOW);
+      delay(1000);
+    }
+  } else {
+    if (TimeDelay() >= 3) digitalWrite(buzzerPin, HIGH);
+    if (TimeDelay() >= 4) digitalWrite(motorPin, HIGH);
+  }
+}
+
+int TimeDelay() {
+  long t = millis() - time;
+  t = t / 1000;
+  return t;
+}
